1e82f226106133074f2ef67bd4473cf634900b25
chooper9 <cdhooper912@gmail.com>
2015-04-02 00:25:12 -0500

    Changed FingerView to use a map instead of two lists

--- selendroid-test-app/src/main/java/io/selendroid/testapp/view/FingerView.java
+++ selendroid-test-app/src/main/java/io/selendroid/testapp/view/FingerView.java
@@ -6,7 +6,7 @@ import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Paint canvas view for manual testing on touch gestures.
@@ -16,8 +16,7 @@ import java.util.ArrayList;
  */
 public class FingerView extends View {
   private Paint paint;
-  private ArrayList<Path> paths;
-  private ArrayList<Integer> mPointers;
+  private HashMap<Integer, Path> pointerPathMap;
 
   public FingerView(Context context, AttributeSet attrs) {
     super(context, attrs);
@@ -25,39 +24,31 @@ public class FingerView extends View {
     paint.setColor(Color.WHITE);
     paint.setStyle(Paint.Style.STROKE);
     paint.setStrokeWidth(12);
-    paths = new ArrayList<Path>();
-    mPointers = new ArrayList<Integer>();
+    pointerPathMap = new HashMap<Integer, Path>();
   }
 
   @Override
   public boolean onTouchEvent(MotionEvent event) {
-    int id = event.getPointerId(event.getActionIndex());
-    int idx;
+    // A pointer's index may change throughout touch events, so track id
+    int idx = event.getActionIndex();
+    int id = event.getPointerId(idx);
     switch (event.getActionMasked()) {
       case MotionEvent.ACTION_DOWN:
       case MotionEvent.ACTION_POINTER_DOWN:
-        mPointers.add(id);
         Path pth = new Path();
-        pth.moveTo(event.getX(event.getActionIndex()), event.getY(event.getActionIndex()));
-        paths.add(pth);
+        pth.moveTo(event.getX(idx), event.getY(idx));
+        pointerPathMap.put(id, pth);
         break;
       case MotionEvent.ACTION_MOVE:
-        idx = 0;
-        for(Integer i : mPointers) {
-          paths.get(idx).lineTo(event.getX(idx), event.getY(idx));
-          idx++;
+        // Update all pointers since ACTION_MOVE events don't provide a pointer index
+        for(HashMap.Entry<Integer, Path> entry : pointerPathMap.entrySet()) {
+          idx = event.findPointerIndex(entry.getKey());
+          entry.getValue().lineTo(event.getX(idx), event.getY(idx));
         }
         break;
       case MotionEvent.ACTION_UP:
       case MotionEvent.ACTION_POINTER_UP:
-        idx = 0;
-        for(Integer i : mPointers) {
-          if(mPointers.get(idx) == id)
-            break;
-          idx++;
-        }
-        mPointers.remove(idx);
-        paths.remove(idx);
+        pointerPathMap.remove(id);
         break;
     }
     invalidate();
@@ -66,7 +57,7 @@ public class FingerView extends View {
 
   @Override
   protected void onDraw(Canvas canvas) {
-    for(Path p : paths) {
+    for(Path p : pointerPathMap.values()) {
       canvas.drawPath(p, paint);
     }
   }
