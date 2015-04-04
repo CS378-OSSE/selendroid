805bef7c27be6e7e5674f961ec77107d84393e21
colindmurray <murray.colin43@gmail.com>
2015-04-03 23:52:16 -0500

    revised dependency structure.  Seems to be working on this machine.

--- third-party/com.android.support-v4/mvnInstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-mvn install:install-file -Dfile=classes.jar \
-  -DpomFile=support-v4-21.0.3.pom -Dpackaging=jar
-
-mvn install:install-file -Dfile=support-annotations-21.0.3.jar \
-  -DgroupId=com.android.support -DartifactId=support-annotations \
-  -Dversion=21.0.3 -Dpackaging=jar
