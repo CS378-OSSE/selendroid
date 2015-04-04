3ef8b29ed8644d2151c6a2708c364b578c30c426
colindmurray <murray.colin43@gmail.com>
2015-04-04 00:01:33 -0500

    forgot to add renamed support-v4 folder to library

---
+++ third-party/support-v4/mvnInstall.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+#mvn install:install-file -Dfile=classes.jar \
+#  -DpomFile=support-v4-21.0.3.pom -Dpackaging=jar
+
+#mvn install:install-file -Dfile=support-annotations-21.0.3.jar \
+#  -DgroupId=com.android.support -DartifactId=support-annotations \
+#  -Dversion=21.0.3 -Dpackaging=jar
+
+
+mvn install:install-file -Dfile=classes.jar \
+  -DgroupId=com.google.android -DartifactId=support-v4 \
+  -Dversion=21.0.3 -Dpackaging=jar
